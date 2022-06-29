6dac35ca421b5436d100094922dfcc6420b607ba
Stats_off <statsoffnow@gmail.com>
2022-06-29 19:12:14 +0200

    Add files via upload

---
+++ game.rb
@@ -0,0 +1,19 @@
+=begin
+    Get my number game.
+    Random number picker for people waiting in lines.
+    Written and created by Karolina Koziara
+=end
+
+puts "Welcome to 'Get My Number game!'"
+print "What's your name? "
+
+input = gets
+
+puts "Welcome #{input}"
+
+
+
+name = "Jim"
+
+puts "hello #{name}"
+
